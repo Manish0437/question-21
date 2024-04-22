716a6ceb6b95a11eecbd5820dbc7cb917d67395b
Manish0437 <86232859+Manish0437@users.noreply.github.com>
2024-04-22 19:04:39 +0530

    second-commit

--- src/components/VaccinationByAge/index.js
+++ src/components/VaccinationByAge/index.js
@@ -7,30 +7,28 @@ const VaccinationByAge = props => {
   return (
     <div className="age-cont">
       <h1 className="graph-titles">Vaccination by Age</h1>
-      <ResponsiveContainer width="100%" height={300}>
-        <PieChart>
-          <Pie
-            cx="70%"
-            cy="40%"
-            data={data}
-            startAngle={0}
-            endAngle={360}
-            innerRadius="0%"
-            outerRadius="70%"
-            dataKey="count"
-          >
-            <Cell name="18-44" fill="#64c2a6" />
-            <Cell name="44-60" fill="#a3df9f" />
-            <Cell name="Above 60" fill="#2d87bb" />
-          </Pie>
-          <Legend
-            iconType="circle"
-            layout="vertical"
-            verticalAlign="middle"
-            align="right"
-          />
-        </PieChart>
-      </ResponsiveContainer>
+      <PieChart width={1000} height={300}>
+        <Pie
+          cx="70%"
+          cy="40%"
+          data={data}
+          startAngle={0}
+          endAngle={360}
+          innerRadius="0%"
+          outerRadius="70%"
+          dataKey="count"
+        >
+          <Cell name="18-44" fill="#64c2a6" />
+          <Cell name="44-60" fill="#a3df9f" />
+          <Cell name="Above 60" fill="#2d87bb" />
+        </Pie>
+        <Legend
+          iconType="circle"
+          layout="vertical"
+          verticalAlign="middle"
+          align="right"
+        />
+      </PieChart>
     </div>
   )
 }
